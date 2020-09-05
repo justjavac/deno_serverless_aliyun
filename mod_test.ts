a564155fe23bcaf9973fbd73607e830f5cb7645e
迷渡 <justjavac@gmail.com>
2020-09-06 01:05:49 +0800

    upgrade std@0.68.0 (#5)
    * upgrade std@0.68.0
    
    * remove 'v'
    
    * upgrade deno@1.3

--- mod_test.ts
+++ mod_test.ts
@@ -1,4 +1,4 @@
-import { assertEquals } from "https://deno.land/std@v0.57.0/testing/asserts.ts";
+import { assertEquals } from "https://deno.land/std@0.68.0/testing/asserts.ts";
 
 Deno.test("myTestFunction", (): void => {
   assertEquals("foo bar", "foo bar");
