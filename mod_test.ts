ce8a46d7438268d9edaa95cf3e2d90bb658d3636
迷渡 <justjavac@gmail.com>
2020-12-28 12:27:16 +0800

    升级到 1.6.2

--- mod_test.ts
+++ mod_test.ts
@@ -1,4 +1,4 @@
-import { assertEquals } from "https://deno.land/std@0.68.0/testing/asserts.ts";
+import { assertEquals } from "https://deno.land/std@0.82.0/testing/asserts.ts";
 
 Deno.test("myTestFunction", (): void => {
   assertEquals("foo bar", "foo bar");
